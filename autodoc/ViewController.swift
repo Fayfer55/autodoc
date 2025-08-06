2c4d26250c1dbea07733762ebc3dc91c25d052cb
Kirill Faifer <sayfer555@gmail.com>
2025-08-06 15:53:31 +0600

    Initial Commit

---
+++ autodoc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  autodoc
+//
+//  Created by Kirill Faifer on 06.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
