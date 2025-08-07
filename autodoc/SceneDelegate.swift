a638337c9df8485e5b7fca5baed01ad50983da74
Kirill Faifer <sayfer555@gmail.com>
2025-08-07 18:02:40 +0600

    add connection between StartVC & window in SceneDelegate

--- autodoc/SceneDelegate.swift
+++ autodoc/SceneDelegate.swift
@@ -12,7 +12,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        let viewCotroller = StartViewController()
+        let navigationController = UINavigationController(rootViewController: viewCotroller)
+        
+        window = UIWindow(windowScene: scene)
+        window?.rootViewController = navigationController
     }
 
 }
