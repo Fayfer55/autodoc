962e51624adff5637b9f133579015f76bb439f19
Kirill Faifer <sayfer555@gmail.com>
2025-08-12 13:28:17 +0600

    add NSDirectionalEdgeInsets+Static extension

---
+++ autodoc/Extensions/UIKit/NSDirectionalEdgeInsets+Static.swift
@@ -0,0 +1,14 @@
+//
+//  NSDirectionalEdgeInsets+Static.swift
+//  autodoc
+//
+//  Created by Kirill Faifer on 12.08.2025.
+//
+
+import UIKit
+
+extension NSDirectionalEdgeInsets {
+    
+    static let safeArea = NSDirectionalEdgeInsets(top: 16, leading: 16, bottom: 16, trailing: 16)
+    
+}
