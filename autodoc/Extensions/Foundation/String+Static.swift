2f0e3518d9e87faf0933df345002f6a9b620a14f
Kirill Faifer <sayfer555@gmail.com>
2025-08-15 18:57:48 +0600

    add String+Static extension

---
+++ autodoc/Extensions/Foundation/String+Static.swift
@@ -0,0 +1,14 @@
+//
+//  String+Static.swift
+//  autodoc
+//
+//  Created by Kirill Faifer on 15.08.2025.
+//
+
+import Foundation
+
+extension String {
+    
+    static let mockCarURL = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.mbusa.com%2Fen%2Fall-vehicles&psig=AOvVaw0g4pPQYVVgt7ce1G-Xs93w&ust=1755348833534000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCNibzpDujI8DFQAAAAAdAAAAABAE"
+    
+}
