b556532a922d993f170716eea191c6c64f7a544b
Kirill Faifer <sayfer555@gmail.com>
2025-11-12 15:49:49 +0600

    change mockCarURL in string+static extension

--- autodoc/Extensions/Foundation/String+Static.swift
+++ autodoc/Extensions/Foundation/String+Static.swift
@@ -9,6 +9,6 @@ import Foundation
 
 extension String {
     
-    static let mockCarURL = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.mbusa.com%2Fen%2Fall-vehicles&psig=AOvVaw0g4pPQYVVgt7ce1G-Xs93w&ust=1755348833534000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCNibzpDujI8DFQAAAAAdAAAAABAE"
+    static let mockCarURL = "https://file.autodoc.ru/news/avto-novosti/4057748062_1.jpg"
     
 }
