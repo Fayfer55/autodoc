ebfa33f07e267e3f3642ef3c8629acdacd8c17b1
Kirill Faifer <sayfer555@gmail.com>
2025-11-21 14:43:21 +0600

    add methods for UICollectionReusableView into UICollectionView+ReuseIdentifiable extension

--- autodoc/Extensions/UIKit/UICollectionView+ReuseIdentifiable.swift
+++ autodoc/Extensions/UIKit/UICollectionView+ReuseIdentifiable.swift
@@ -7,6 +7,8 @@
 
 import UIKit.UICollectionView
 
+// MARK: - UICollectionViewCell
+
 extension UICollectionView {
 
     func register<T: UICollectionViewCell>(cellType: T.Type) where T: ReuseIdentifiable {
@@ -34,12 +36,46 @@ extension UICollectionView {
 
 }
 
+// MARK: - UICollectionReusableView
+
+extension UICollectionView {
+    
+    func register<T: UICollectionReusableView>(viewType: T.Type, viewOfKind: CollectionReusableKind) where T: ReuseIdentifiable {
+        register(viewType.self, forSupplementaryViewOfKind: viewOfKind.rawValue, withReuseIdentifier: viewType.reuseId)
+    }
+    
+    func dequeueReusableView<T: UICollectionReusableView>(
+        withViewType type: T.Type = T.self,
+        viewOfKind: String,
+        for indexPath: IndexPath
+    ) -> T where T: ReuseIdentifiable {
+        guard let view = dequeueReusableSupplementaryView(
+            ofKind: viewOfKind, withReuseIdentifier: type.reuseId, for: indexPath
+        ) as? T else {
+            fatalError(dequeueError(reuseId: type.reuseId))
+        }
+        return view
+    }
+    
+    func supplementaryView<T: UICollectionReusableView>(
+        withViewType type: T.Type = T.self,
+        viewOfKind: CollectionReusableKind,
+        at indexPath: IndexPath
+    ) -> T where T: ReuseIdentifiable {
+        guard let view = supplementaryView(forElementKind: viewOfKind.rawValue, at: indexPath) as? T else {
+            fatalError(dequeueError(reuseId: type.reuseId))
+        }
+        return view
+    }
+    
+}
+
 // MARK: - Dequeue Error Method
 
 extension UICollectionView {
 
     private func dequeueError(reuseId: String) -> String {
-        "Couldn't dequeue cell with identifier \(reuseId)"
+        "Couldn't dequeue view with identifier \(reuseId)"
     }
 
 }
